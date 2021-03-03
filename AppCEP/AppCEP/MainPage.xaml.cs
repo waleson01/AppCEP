9ae6f35749b8c5cc095fa1a258bfb669e5639d61
waleson01 <waleson01@users.noreply.github.com>
2021-03-03 19:30:48 -0300

    Completo

--- AppCEP/AppCEP/MainPage.xaml.cs
+++ AppCEP/AppCEP/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppCEP.servico;
+using AppCEP.servico.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,11 +10,60 @@ using Xamarin.Forms;
 
 namespace AppCEP
 {
+        [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            ImgLogo.Source = ImageSource.FromResource("AppCEP.img.Logo.png");
         }
+
+        private void btnBuscar_Clicked(object sender, EventArgs e)
+        {
+            //retirar os espaços vazio do entry
+            String cep = txtCEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServicos.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {2} \nBairro: {3} \nCidade: {0}-{1}", end.localidade, end.uf, end.logadouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO!", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO", ex.Message, "OK");
+                }
+            }
+
+        }
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP invalido! O CEP deve conter 8 caracteres.", "OOK");
+                valido = false;
+            }
+            int novoCEP = 0;
+            if(!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("Erro", "CEP invalido! o CEP deve ser composto apenas por número.", "OK");
+                valido = false;
+            }
+            return valido;
+
+        }
+
+
     }
 }
---
+++ AppCEP/AppCEP/servico/ViaCEPServicos.cs
@@ -0,0 +1,35 @@
+﻿using AppCEP.servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCEP.servico
+{
+    public class ViaCEPServicos
+    {
+        //string com o endereço do serviço web
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            //propriedade estatica que recebe o endereço
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            //objeto que acessa o resultado do endereço web passado como parametro
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            //objeto do tipo Classe enderço que recebe os dados des-serializados
+            Endereco end = JsonConvert.DeserializeObejto<Endereco>(Conteudo);
+
+            //Caso não haja retorno do serviço Web
+            if (end.cep == null) return null;
+
+            return end;
+
+
+        }
+    }
+}
